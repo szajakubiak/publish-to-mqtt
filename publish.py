8adb2cbda0d0c0a20a7692710a2c523f6ae74613
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2024-12-22 22:16:04 +0100

    Code formatting

--- publish.py
+++ publish.py
@@ -9,12 +9,13 @@ MQTT_USERNAME = passwords.username
 MQTT_PASSWORD = passwords.password
 DEBUG = False
 
+
 def send_mqtt_message(topic, command):
     client = mqtt.Client()
-    
+
     if MQTT_USERNAME and MQTT_PASSWORD:
         client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-    
+
     try:
         client.connect(MQTT_BROKER, MQTT_PORT, 60)
         client.publish(topic, command)
@@ -26,10 +27,17 @@ def send_mqtt_message(topic, command):
             print(f"Failed to send MQTT message: {e}")
         sys.exit(1)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MQTT sockets controller")
     parser.add_argument("--topic", "-t", required=True, help="Target topic")
-    parser.add_argument("--command", "-c", choices=["ON", "OFF"], required=True, help="Command to send (ON or OFF)")
+    parser.add_argument(
+        "--command",
+        "-c",
+        choices=["ON", "OFF"],
+        required=True,
+        help="Command to send (ON or OFF)",
+    )
     args = parser.parse_args()
     topic = args.topic
     command = args.command
